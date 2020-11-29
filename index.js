4bae968c0bd736016332523ea49f2109c040aa5a
radwan <radwan.chaieb@gmail.com>
2020-11-29 19:06:42 +0100

    create

---
+++ index.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const express = require("express");
+const path = require("path");
+const app = express();
+
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res)=>{
+try{
+const files = fs.readdirSync("/home/xzerad/");
+var dir = {file:files}
+res.render("index", dir);
+}catch(err){
+console.log(err);
+}
+
+});
+
+app.get("/download/:path", (req, res)=>{
+
+	const file = `/home/xzerad/${req.params.path}`
+	res.download(file);
+
+});
+
+app.listen(8000, function(err){
+	if(err) throw err;
+	console.log("server created successfully");
+	console.log("http://localhost:8000")
+});
