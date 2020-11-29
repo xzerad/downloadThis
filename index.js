4c2967c0a008bf5e9eabed7f6ab2c80c6fff9d89
radwan <radwan.chaieb@gmail.com>
2020-11-29 21:34:44 +0100

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res)=>{
